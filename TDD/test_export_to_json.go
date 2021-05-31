095bb9485dc58a0b45b48ba439ef1f3a264ef9a4
Simon Aertssen <simonaertssen@corti-laptop.lan>
2021-05-31 14:45:53 +0200

    Finally exported to JSON successfully

--- TDD/test_export_to_json.go
+++ TDD/test_export_to_json.go
@@ -8,30 +8,31 @@ import (
 
 
 type User struct {
-    name     string
-    digest   string
-    balance  float32
+    Name     string
+    Digest   string
+    Balance  float32
 }
 
 
 func main() {
     U1 := User {
-                name: "Sean",
-                digest: "40d18d5a7ae85f9597a40f1306041fd1", 
-                balance: 50.23,
+                Name:   "Sean",
+                Digest: "40d18d5a7ae85f9597a40f1306041fd1", 
+                Balance: 50.23,
                 }
     U2 := User {
-                name: "Donnie",
-                digest: "6f171d413bee711762beff4276595068", 
-                balance: 14572.291,
+                Name:   "Donnie",
+                Digest: "6f171d413bee711762beff4276595068", 
+                Balance: 14572.291,
                 }
-    var all_users = []User {U1, U2}
-    fmt.Println(all_users)
 
-    data, error := json.Marshal(all_users)
+    var all_users = []User {U1, U2}
+    //fmt.Println(all_users)
+
+    file, error := json.MarshalIndent(all_users, "", " ")
     if error != nil {
         fmt.Println("error:", error)
     }
 
-    error = ioutil.WriteFile("test_users.json", data, 0644)
+    error = ioutil.WriteFile("test_users.json", file, 0644)
 }
