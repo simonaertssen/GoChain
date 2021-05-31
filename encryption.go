8b3ac7ed2df5faa98cb264d466b8b3116af46ba5
Simon Aertssen <simonaertssen@corti-laptop.lan>
2021-05-31 16:08:11 +0200

    Making sure we can make the initial state

--- encryption.go
+++ encryption.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-    "fmt"
-    "encoding/json"
-    "io/ioutil"
     "crypto/md5"
 )
 
-func main() {
-    // Read the state file
-    data, error := ioutil.ReadFile("data/state.json")
-    if error != nil {
-      fmt.Print(err)
-    }
-
-    // If users are not encrypted yet, encrypt
-    s := "Simon"
+func md5digest() {
     digest := md5.Sum([]byte(s))
-
-    fmt.Printf("%x\n", digest)
 }
